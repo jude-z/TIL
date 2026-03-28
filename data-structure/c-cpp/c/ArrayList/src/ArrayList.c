d59db3305c3d21560b4fe0bb0dd17800e2588966
luvc++ <ccccssung@gmail.com>
2026-03-28 20:14:32 +0900

    c/c++ ArrayList

---
+++ data-structure/c-cpp/c/ArrayList/src/ArrayList.c
@@ -0,0 +1,9 @@
+#include "ArrayList.h"
+void ListInit(List* plist) {
+    plist -> numOfData = 0;
+    plist -> curPosition = -1;
+}
+
+void LInsert(List* plist, LData data) {
+
+}
---
+++ data-structure/c-cpp/c/HanoiTower/src/HanoiTower.h
@@ -0,0 +1,5 @@
+#ifndef HANOI_TOWER_H
+#define HANOI_TOWER_H
+
+void HanoiTowerMove(int num, char from, char by, char to);
+#endif
