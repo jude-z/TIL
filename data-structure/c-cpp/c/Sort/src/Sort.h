ef15eb4ced0991eaa2a76cb94314867bf5c59db9
jude-z <ccccssung@gmail.com>
2026-04-28 17:37:55 +0900

    # 2026.04.28
    ## 🛠 What I Learned
    - BubbleSort, SelectSort c로 구현

---
+++ data-structure/c-cpp/c/Sort/src/Sort.h
@@ -0,0 +1,8 @@
+#ifndef SORT_H
+#define SORT_H
+
+typedef int Data;
+void bubbleSort(Data** arr_ptr, int len);
+void selectSort(Data** arr_ptr, int len);
+void insertSort(Data** arr_ptr, int len);
+#endif
